609635cd864a724cd0a2e55690f5b42f32e3cd11
simonostensson2001 <simonostensson2001@users.noreply.github.com>
2020-05-19 13:39:09 +0200

    loggboken

--- loggbok.py
+++ loggbok.py
@@ -1,23 +1,32 @@
 system = True
 
+try:
+    f = open("loggar.txt", "r")
+    print('laddade in logg filen')
+    f.close()
+except:
+    f = open("loggar.txt", "w")
+    print('skapar logg filen')
+    f.close()
+
+
 while system:
-    print ('''
-    1.Visa alla loggar
-    2.Ny logg
-    3.Spara loggar
-    4.Avsluta
-    ''')
+    print ('1. Visa alla loggar\n2.Ny logg\n3.Spara loggar till fil\n4.Avsluta')
 try:
     val = int(input("Vad vill du göra?"))
 except:
     val = 0
 if val == 1:
     print("Visar loggar")
+    f = open("loggar.txt", "r")
+    print(f.read()) 
+    f.close()
 elif val == 2:
-    print("Skapar ny logg")
+    print("ny logg")
 elif val == 3:
     print("Sparar logg")
 elif val == 4:
     print("Avslutar")
+    system = False 
 else:
     print("Prova välj av följande 1,2,3,4.")
