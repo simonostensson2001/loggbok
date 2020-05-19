031d3f1a5d67ce4ac67bb396b20b96d4b744cee5
simonostensson2001 <simonostensson2001@users.noreply.github.com>
2020-05-19 13:10:15 +0200

    loggboken

--- loggbok.py
+++ loggbok.py
@@ -1,14 +1,23 @@
-system=true
+system = True
+
 while system:
-    print ("
+    print ('''
     1.Visa alla loggar
     2.Ny logg
-    3.Spara loggar till fil
+    3.Spara loggar
     4.Avsluta
-    ")
-system=input("Vad vill du göra?")
-if system=="1":
+    ''')
+try:
+    val = int(input("Vad vill du göra?"))
+except:
+    val = 0
+if val == 1:
     print("Visar loggar")
-elif system=="2":
+elif val == 2:
     print("Skapar ny logg")
-elif system=="3":
+elif val == 3:
+    print("Sparar logg")
+elif val == 4:
+    print("Avslutar")
+else:
+    print("Prova välj av följande 1,2,3,4.")
