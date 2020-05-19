e9e62ef7e59a6548158f6fb8728eda793b345e65
simonostensson2001 <simonostensson2001@users.noreply.github.com>
2020-05-19 12:48:11 +0200

    loggbok

--- loggbok.py
+++ loggbok.py
@@ -1,4 +1,14 @@
-menu = 0
-
-while menu == 0:
-    
+system=true
+while system:
+    print ("
+    1.Visa alla loggar
+    2.Ny logg
+    3.Spara loggar till fil
+    4.Avsluta
+    ")
+system=input("Vad vill du göra?")
+if system=="1":
+    print("Visar loggar")
+elif system=="2":
+    print("Skapar ny logg")
+elif system=="3":
